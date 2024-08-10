c87f01f256f013d3272cee6daf1b9780183773d9
Martín Acosta <m@sis.uy>
2024-08-10 00:31:33 -0300

    Add: i18n, base para traducción

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,23 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./app/App.jsx";
 import "./index.css";
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+
+import * as es from "./locales/es.json";
+
+i18n.use(initReactI18next).init({
+  resources: {
+    es: {
+      translation: es,
+    },
+  },
+  lng: "es",
+  fallbackLng: "es",
+  interpolation: {
+    escapeValue: false,
+  },
+});
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
